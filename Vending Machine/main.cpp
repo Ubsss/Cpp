3c1ba60bc9db72d14b92da8ed2d3fe3f79028daf
uboh <ubohchuks@gmail.com>
2018-11-20 23:21:55 -0600

    added vending machine project

---
+++ Vending Machine/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Vending Machine
+//
+//  Created by Uchechukwu Uboh on 11/20/18.
+//  Copyright © 2018 Uchechukwu Uboh. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    cout << "Hello, World!\n";
+    return 0;
+}
